b161051049426dd4b95ef566a0ced255f3f98271
gmirandacarvalho <guilherme.carvalho@r2sis.com.br>
2024-04-29 14:29:45 -0300

    WIP

---
+++ assets/js/owl.js
@@ -0,0 +1,17 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:true,
+    autoWidth:true,
+    items:3
+    // responsive:{
+    //     0:{
+    //         items:1
+    //     },
+    //     300:{
+    //         items:2
+    //     },
+    //     600:{
+    //         items:3
+    //     },
+    // },
+})
