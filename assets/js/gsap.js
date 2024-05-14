eca6922e9d543eaac076a52fe7d3d9d43a3a21f0
Guilherme-M-Carvalho <105821481+Guilherme-M-Carvalho@users.noreply.github.com>
2024-05-13 22:24:18 -0300

    ajustes

--- assets/js/gsap.js
+++ assets/js/gsap.js
@@ -42,7 +42,8 @@ $(document).ready(function () {
         scrollTrigger: {
             trigger: '.about-us',
             // markers: true,
-            start: "0px 80%",
+            start: 'bottom bottom',
+
         },
         x: -(window.innerWidth / 2),
         opacity: 0,
@@ -52,30 +53,32 @@ $(document).ready(function () {
     const widthX = (window.innerWidth / 2)
 
     // animation best-services
-    if (widthX > 531) {
+    // if (widthX > 531) {
         gsap.from('.best-services-body-content', {
             ease: "expo.out",
             scrollTrigger: {
                 trigger: '.best-service-image',
-                start: "0px 80%",
+                start: 'bottom bottom',
+
                 // markers: true
             },
             x: widthX,
             opacity: 0,
             duration: 2,
-            rotation: 90
         })
-    }
+    // }
+
 
 
     // animation our-advantages
     gsap.from('.left-advantages', {
         scrollTrigger: {
             trigger: '.our-advantages',
+            start: 'bottom bottom',
             // markers: true,
-            start: "0px 80%",
+            // start: "0px 80%",
         },
-        x: -200,
+        x: -(window.innerWidth / 2),
         opacity: 0,
         duration: 1
     })
@@ -86,11 +89,106 @@ $(document).ready(function () {
             trigger: '.our-advantages',
             // markers: true,
             start: "0px 80%",
+            start: 'bottom bottom',
         },
         x: (window.innerWidth / 2),
         opacity: 0,
         duration: 1
     })
+
+    // animation our-advantages
+    gsap.from('.result-guarantee-body', {
+        scrollTrigger: {
+            trigger: '.result-guarantee',
+            // markers: true,
+            start: 'bottom bottom',
+        },
+        x: -(window.innerWidth / 2),
+        opacity: 0,
+        duration: 1
+    })
+    // animation our-advantages
+    gsap.from('.card-representation', {
+        scrollTrigger: {
+            trigger: '.card-representation',
+            // markers: true,
+            start: 'center bottom',
+        },
+        x: -(window.innerWidth / 2),
+        opacity: 0,
+        duration: 1
+    })
+    gsap.from('.card-aliment', {
+        scrollTrigger: {
+            trigger: '.card-aliment',
+            // markers: true,
+            start: 'center bottom',
+        },
+        x: -(window.innerWidth / 2),
+        opacity: 0,
+        duration: 1
+    })
+    gsap.from('.card-pneu', {
+        scrollTrigger: {
+            trigger: '.card-pneu',
+            // markers: true,
+            start: 'center bottom',
+        },
+        x: (window.innerWidth / 2),
+        opacity: 0,
+        duration: 1
+    })
+    gsap.from('.card-oleo', {
+        scrollTrigger: {
+            trigger: '.card-oleo',
+            // markers: true,
+            start: 'center bottom',
+        },
+        x: (window.innerWidth / 2),
+        opacity: 0,
+        duration: 1
+    })
+
+    gsap.from('.what-offer-one', {
+        scrollTrigger: {
+            trigger: '.what-offer-one',
+            // markers: true,
+            start: 'bottom bottom',
+        },
+        x: -(window.innerWidth / 2),
+        opacity: 0,
+        duration: 2
+    })
+    gsap.from('.what-offer-two', {
+        scrollTrigger: {
+            trigger: '.what-offer-two',
+            // markers: true,
+            start: 'bottom bottom',
+        },
+        x: -(window.innerWidth / 2),
+        opacity: 0,
+        duration: 1.5
+    })
+    gsap.from('.what-offer-three', {
+        scrollTrigger: {
+            trigger: '.what-offer-three',
+            // markers: true,
+            start: 'bottom bottom',
+        },
+        x: -(window.innerWidth / 2),
+        opacity: 0,
+        duration: 1
+    })
+    // gsap.from('.card-right', {
+    //     scrollTrigger: {
+    //         trigger: '.card-left',
+    //         // markers: true,
+    //         start: 'bottom bottom',
+    //     },
+    //     x: (window.innerWidth / 2),
+    //     opacity: 0,
+    //     duration: 1
+    // })
 
     // animation best-services
     // gsap.from('.best-services-body', {
