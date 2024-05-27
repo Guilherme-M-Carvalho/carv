30da014ccbc8457cffb7b809ba8b4fe1be09f44e
gmirandacarvalho <102979165+gmirandacarvalho@users.noreply.github.com>
2024-05-27 11:34:31 -0300

    remove console

--- assets/js/images.js
+++ assets/js/images.js
@@ -1,7 +1,6 @@
 const w = window.innerWidth;
 $(document).ready(() => {
     if(w > 572) {
-        console.log({w});
         $("#break-img").attr("src", "./assets/images/150/breakRepair.webp")
         $("#oil-img").attr("src", "./assets/images/150/oil.webp")
         $("#suspension-img").attr("src", "./assets/images/150/suspension.webp")
