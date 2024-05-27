d455c5568cd4801c67a98bf753e874509b1bbb85
gmirandacarvalho <102979165+gmirandacarvalho@users.noreply.github.com>
2024-05-27 15:22:43 -0300

    min js

--- assets/js/owl.js
+++ assets/js/owl.js
@@ -5,15 +5,4 @@ $('.owl-carousel').owlCarousel({
     autoplay: true,
     autoplayTimeout: 5000,
     items:5
-    // responsive:{
-    //     0:{
-    //         items:1
-    //     },
-    //     300:{
-    //         items:2
-    //     },
-    //     600:{
-    //         items:3
-    //     },
-    // },
 })
